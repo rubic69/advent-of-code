0bf7d3c79a8f86e90157dc00ddd41be0d04778ea
Karolis Pavilionis <pavilionis.k@gmail.com>
2022-12-02 15:19:42 +0200

    2022 day 02 > refined solution of part two & refactored part one

--- src/main/java/com/kp/aoc/year22/day02/Day02Refined.java
+++ src/main/java/com/kp/aoc/year22/day02/Day02Refined.java
@@ -2,8 +2,6 @@ package com.kp.aoc.year22.day02;
 
 import com.kp.aoc.Solver;
 
-import java.util.List;
-
 public class Day02Refined implements Solver<Day02.Input, Integer> {
 
     private static final String OPPONENT_SHAPES = "ABC";
@@ -12,42 +10,79 @@ public class Day02Refined implements Solver<Day02.Input, Integer> {
     private static final int DRAW_SCORE = 3;
     private static final int WIN_SCORE = 6;
 
-    /**
-     *                    +----------+-----------+--------------+
-     *                    | Rock (A) | Paper (B) | Scissors (C) |
-     *                    +----------+-----------+--------------+
-     *                    | 0        |  1        |  2           |
-     * +--------------+---+----------+-----------+--------------+
-     * | Rock (X)     | 0 | 0        | -1        | -2           |
-     * +--------------+---+----------+-----------+--------------+
-     * | Paper (Y)    | 1 | 1        |  0        | -1           |
-     * +--------------+---+----------+-----------+--------------+
-     * | Scissors (Z) | 2 | 2        |  1        |  0           |
-     * +--------------+---+----------+-----------+--------------+
-     * <p>
-     * Draw -> 0
-     * Win -> -2 & 1
-     * Lose -> anything else (-1, 2)
-     */
+    private static final String OUTCOME_LOSE = "X";
+    private static final String OUTCOME_DRAW = "Y";
+    private static final String OUTCOME_WIN = "Z";
+
+    //                             ╔═════════════════════════════════════╗
+    //                             ║               Opponent              ║
+    //                             ╠══════════╦═══════════╦══════════════╣
+    //                             ║ (A) Rock ║ (B) Paper ║ (C) Scissors ║
+    //                     ╔═══════╬══════════╬═══════════╬══════════════╣
+    //                     ║ Index ║ 0        ║ 1         ║ 2            ║
+    // ╔════╦══════════════╬═══════╬══════════╬═══════════╬══════════════╣
+    // ║    ║ (X) Rock     ║ 0     ║ 0 (DRAW) ║ 2 (LOSE)  ║ 1 (WIN)      ║
+    // ║    ╠══════════════╬═══════╬══════════╬═══════════╬══════════════╣
+    // ║ Me ║ (Y) Paper    ║ 1     ║ 1 (WIN)  ║ 0 (DRAW)  ║ 2 (LOSE)     ║
+    // ║    ╠══════════════╬═══════╬══════════╬═══════════╬══════════════╣
+    // ║    ║ (Z) Scissors ║ 2     ║ 2 (LOSE) ║ 1 (WIN)   ║ 0 (DRAW)     ║
+    // ╚════╩══════════════╩═══════╩══════════╩═══════════╩══════════════╝
+
     @Override
     public Integer solvePartOne(Day02.Input input) {
-        List<String[]> allHandShapes = input.handShapes();
-
-        return allHandShapes.stream()
+        return input.handShapes().stream()
                 .map(this::calculateMatchScore)
                 .reduce(0, Integer::sum);
     }
 
-    private Integer calculateMatchScore(String[] handShapes) {
-        int opponentPowerIndex = OPPONENT_SHAPES.indexOf(handShapes[0]);
-        int myPowerIndex = MY_SHAPES.indexOf(handShapes[1]);
+    private Integer calculateMatchScore(String[] handShapePair) {
+        int opponentPowerIndex = OPPONENT_SHAPES.indexOf(handShapePair[0]);
+        int myPowerIndex = MY_SHAPES.indexOf(handShapePair[1]);
 
         int shapeScore = myPowerIndex + 1;
 
-        return switch (myPowerIndex - opponentPowerIndex) {
-            case -2, 1 -> WIN_SCORE + shapeScore;
+        int roundOutcome = Math.floorMod(myPowerIndex - opponentPowerIndex, 3);
+
+        return switch (roundOutcome) {
+            case 1 -> WIN_SCORE + shapeScore;
             case 0 -> DRAW_SCORE + shapeScore;
-            default -> shapeScore;
+            case 2 -> shapeScore;
+            default -> throw new UnsupportedOperationException("Should never happen");
         };
     }
+
+    // Win -> -1
+    // Lose -> +1
+    // Draw -> Doesn't matter
+    //
+    //  ╔═══════╦══════════╦═══════════╦══════════════╗
+    //  ║       ║ (A) Rock ║ (B) Paper ║ (C) Scissors ║
+    //  ╠═══════╬══════════╬═══════════╬══════════════╣
+    //  ║ Index ║ 0        ║ 1         ║ 2            ║
+    //  ╠═══════╬══════════╬═══════════╬══════════════╣
+    //  ║ Win   ║ 2        ║ 0         ║ 1            ║
+    //  ╠═══════╬══════════╬═══════════╬══════════════╣
+    //  ║ Lose  ║ 1        ║ 2         ║ 0            ║
+    //  ╠═══════╬══════════╬═══════════╬══════════════╣
+    //  ║ Draw  ║ 0        ║ 1         ║ 2            ║
+    //  ╚═══════╩══════════╩═══════════╩══════════════╝
+
+    @Override
+    public Integer solvePartTwo(Day02.Input input) {
+        return input.handShapes().stream()
+                .map(this::calculateCleverMatchScore)
+                .reduce(0, Integer::sum);
+    }
+
+    private Integer calculateCleverMatchScore(String[] handShapeAndOutcomePair) {
+        int opponentPowerIndex = OPPONENT_SHAPES.indexOf(handShapeAndOutcomePair[0]);
+
+        return switch (handShapeAndOutcomePair[1]) {
+            case OUTCOME_LOSE -> Math.floorMod(opponentPowerIndex - 1, 3) + 1;
+            case OUTCOME_DRAW -> DRAW_SCORE + opponentPowerIndex + 1;
+            case OUTCOME_WIN -> WIN_SCORE + Math.floorMod(opponentPowerIndex + 1, 3) + 1;
+            default -> throw new UnsupportedOperationException("Should never happen");
+        };
+    }
+
 }
--- src/test/java/com/kp/aoc/year22/day02/Day02RefinedTest.java
+++ src/test/java/com/kp/aoc/year22/day02/Day02RefinedTest.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day02RefinedTest {
 
@@ -25,6 +25,18 @@ class Day02RefinedTest {
     void test_part_1_input() throws IOException {
         List<String[]> input = readInput("part_1_input");
         assertEquals(13924, solver.solvePartOne(new Day02.Input(input)));
+    }
+
+    @Test
+    void test_part_2_input_sample() throws IOException {
+        List<String[]> input = readInput("part_1_sample");
+        assertEquals(12, solver.solvePartTwo(new Day02.Input(input)));
+    }
+
+    @Test
+    void test_part_2_input() throws IOException {
+        List<String[]> input = readInput("part_2_input");
+        assertEquals(13448, solver.solvePartTwo(new Day02.Input(input)));
     }
 
     private List<String[]> readInput(String fileName) throws IOException {
